a8402f856be6542b38aa3f17a7efa766b37ff39b
shinicode <nakinakou@gmail.com>
2019-06-18 17:56:14 +0300

    git push

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -4,8 +4,33 @@ const name = `Alex`
 const userAge = 21 
 
 const user = {
-    name: name,
+    name,
     age : userAge,
-    location : "athens"
+    location : "Athens"
 }
 console.log(user);
+
+//* Object destructuring 
+
+
+const product = {
+    label:'Red notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined
+}
+
+// const label = product.label
+// const stock = product.stock
+
+// const {label, price, rating = 5} = product
+// console.log(label);
+// console.log(price);
+// console.log(rating);
+
+
+const transaction = (type, {label, stock}) => {
+    console.log(type , stock, label);
+}
+
+transaction('order', product)
