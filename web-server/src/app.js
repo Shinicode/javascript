1e5d87042b0c90d68d88b26b2c21bc1bc743cc24
shinicode <nakinakou@gmail.com>
2019-06-26 20:09:19 +0300

    git push

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,44 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+//* Define paths for express config
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../src/templates')
+
+//* Setup handlebars engine and views location
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+//* Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title:'Weather',
+        name : 'alex'
+
+    })
+})
+
+app.get('/about', (req, res ) => {
+    res.render('about', {
+        name:'nak'
+    })
+
+})
+
+
+app.get('/help', (req,res) => {
+    res.render('help', {
+        help: 'This is my help message'
+    })
+})
+
 
 app.get('/weather', (req,res) => {
     res.send({
