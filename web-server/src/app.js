24cf85a82190bb39d0584e9e99d0ac638cecaf0a
shinicode <nakinakou@gmail.com>
2019-06-29 19:45:31 +0300

    git push

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -47,12 +47,27 @@ app.get('/help', (req,res) => {
 
 
 app.get('/weather', (req,res) => {
-    res.send({
+    res.render({
         forecast: 'It is snowing',
         location: 'Philadelphia'
     })
 })
 
+app.get('/help/*', (req,res) => {
+    res.render('404',{
+        title:'404',
+        name : 'Shinicode',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req,res) => {
+    res.render('404', {
+        title :'404',
+        name: 'Shinicode',
+        errorMessage: 'Page not found.'
+    })
+})
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.');
