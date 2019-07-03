bd70c9eb54019fcec3b58bab5e3940a03bda4574
shinicode <nakinakou@gmail.com>
2019-07-03 17:44:20 +0300

    git push

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -47,11 +47,28 @@ app.get('/help', (req,res) => {
 
 
 app.get('/weather', (req,res) => {
-    res.render({
+    if(!req.query.address){
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+    res.send({
         forecast: 'It is snowing',
-        location: 'Philadelphia'
+        location : 'Philadelphia',
+        address : req.query.address
     })
 })
+
+app.get('/products', (req, res) => {
+    if(!req.query.search){
+       return  res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    res.send({
+        products : []
+    })
+}) 
 
 app.get('/help/*', (req,res) => {
     res.render('404',{
